b9e4ca33f42ee5bb51d10de0a73ccb6cf2a47cd5
Stephane Gagnon <stephane.gagnon00@gmail.com>
2017-12-01 11:54:48 -0500

    Ignore
    storing my color scheme here for now...

---
+++ colors.js
@@ -0,0 +1,33 @@
+// terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+cursorColor: '#a89984',
+
+// color of the text
+foregroundColor: '#ebdbb2',
+
+// terminal background color
+backgroundColor: '#32302f',
+
+// border color (window, tabs)
+borderColor: '#689d6a',
+
+// the full list. if you're going to provide the full color palette,
+// including the 6 x 6 color cubes and the grayscale map, just provide
+// an array here instead of a color map object
+colors: {
+  black: '#928374',
+  red: '#d65d0e',
+  green: '#b8bb26',
+  yellow: '#fabd2f',
+  blue: '#83a598',
+  magenta: '#d3869b',
+  cyan: '#8ec07c',
+  white: '#fbf1c7',
+  lightBlack: '#a89984',
+  lightRed: '#fe8019',
+  lightGreen: '#98971a',
+  lightYellow: '#d79921',
+  lightBlue: '#458588',
+  lightMagenta: '#b16286',
+  lightCyan: '#689d6a',
+  lightWhite: '#a89984'
+},
